42c17e4af4c59319b1f2fca727da2b58a5a5300e
daikimiura <daikiii5555@gmail.com>
2020-02-13 00:20:44 +0900

    add: 整数1つをコンパイルするコンパイラを作成

---
+++ dcc.c
@@ -0,0 +1,20 @@
+//
+// Created by 三浦大輝 on 2020/02/12.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "引数の個数が正しくありません\n");
+    return 1;
+  }
+
+  printf(".intel_syntax noprefix\n");
+  printf(".global _main\n");
+  printf("_main:\n");
+  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  ret\n");
+  return 0;
+}
---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+
+  ./dcc "$input" > tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+try 42 42
+
+echo ok
