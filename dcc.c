777913ec585f91f1c3c64c0e76540d385ac51080
daikimiura <daikiii5555@gmail.com>
2020-02-19 00:25:49 +0900

    add: 単項プラスと単項マイナス

--- dcc.c
+++ dcc.c
@@ -174,6 +174,7 @@ Node *new_node_num(int val) {
 // 非終端記号を表す関数のプロトタイプ宣言
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
@@ -190,17 +191,26 @@ Node *expr() {
   }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
   for(;;) {
     if(consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+// unary = ("+" | "-")? primary
+Node *unary() {
+  if(consume('+'))
+    return primary();
+  if(consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
 }
 
 // primary = "(" expr ")" | num
