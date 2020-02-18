c3bd66b299922facc507cf97bf9bc887ff96521c
daikimiura <daikiii5555@gmail.com>
2020-02-19 00:18:25 +0900

    add: 四則演算のできる言語の作成

--- dcc.c
+++ dcc.c
@@ -12,26 +12,6 @@
 // 入力プログラム
 char *user_input;
 
-// トークンの種類
-typedef enum {
-  TK_RESERVED, // 記号
-  TK_NUM, // 整数
-  TK_EOF, // End Of File
-} TokenKind;
-
-typedef struct Token Token;
-
-// トークン型
-struct Token {
-  TokenKind kind; // トークンの種類
-  Token *next; // 次のトークン
-  int val; // kindがTK_NUMの場合、その値
-  char *str; // トークン文字列
-};
-
-// 現在着目しているトークン
-Token *token;
-
 // エラーを報告するための関数
 // printfと同じ引数をとる
 void error(char *fmt, ...) {
@@ -56,6 +36,30 @@ void error_at(char *loc, char *fmt, ...) {
   exit(1);
 
 }
+
+//
+// トークナイザー
+//
+
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM, // 整数
+  TK_EOF, // End Of File
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの種類
+  Token *next; // 次のトークン
+  int val; // kindがTK_NUMの場合、その値
+  char *str; // トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
 
 // 現在のトークンが期待している記号の時には、トークンを1つ読み進めてtrueを返す
 // それ以外の場合にはfalseを返す
@@ -111,7 +115,7 @@ Token *tokenize(char *p){
       continue;
     }
 
-    if (*p=='+' || *p =='-'){
+    if (*p=='+' || *p =='-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -129,33 +133,146 @@ Token *tokenize(char *p){
   return head.next;
 }
 
+//
+// パーザー
+//
+
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs; // 左辺
+  Node *rhs; // 右辺
+  int val; // kindがND_NUMの場合、その値(整数)
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node * node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node * node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+// 非終端記号を表す関数のプロトタイプ宣言
+Node *expr();
+Node *mul();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr() {
+  Node *node = mul();
+
+  for(;;) {
+    if(consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if(consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul() {
+  Node *node = primary();
+  for(;;) {
+    if(consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+// primary = "(" expr ")" | num
+Node *primary() {
+  if(consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_num(expect_number());
+}
+
+void gen(Node *node){
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch(node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      // cqo: RAXに入っている64ビットの値を128ビットに伸ばしてRDXとRAXにセットする
+      printf("  cqo\n");
+      // idiv: 暗黙のうちにRDXとRAXを取って、それを合わせたものを128ビット整数とみなして、それを引数のレジスタの64ビットの値で割り
+      //       商をRAXに、余りをRDXにセットする
+      printf("  idiv rdi\n");
+      break;
+    default:
+      error("パーズエラー");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
 
+  // トークナイズしてパースする
   user_input = argv[1];
-  token = tokenize(argv[1]);
+  token = tokenize(user_input);
+  Node *node = expr();
 
+  // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして最初のmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  // 抽象構文木を下りながらコード生成
+  gen(node);
 
-  // `+ <数>` あるいは `- <数>` というトークンの並びを消費しつつアセンブリを出力
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // スタックトップに式(expr)全体の値が残っているはずなので
+  // それをRAXにロードして関数からの返り値とする
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
